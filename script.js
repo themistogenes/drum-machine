349fe4f52288fb6236f0366656dd22fd4f3fe196
themistogenes <85630327+themistogenes@users.noreply.github.com>
2023-09-20 11:52:38 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+window.addEventListener('keydown', playSound);
+
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+  // console.log(e.keyCode, audio, key);
+
+  if (!audio) return;
+
+  audio.currentTime = 0;
+  audio.play();
+
+  key.classList.add('playing');
+
+  setTimeout(() => {
+    key.classList.remove('playing');
+  }, 100)
+}
